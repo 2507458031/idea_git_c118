71f9cae117a1d100d0d9d3d5104afcda388b573d
zm <2507458031@qq.com>
2018-07-03 21:28:22 +0800

    m1!!

--- src/main/java/com/baizhi/dao/UserDao.java
+++ src/main/java/com/baizhi/dao/UserDao.java
@@ -7,5 +7,9 @@ public class UserDao {
     public static void main(String[] args) {
         System.out.println("Hello");
     }
+    public void m1(){
+
+        System.out.println("m1");
+    }
 
 }

41e56489840a6aefea8a6a1a86413746a5a58980
zm <2507458031@qq.com>
2018-07-03 09:52:35 +0800

    第二次提交项目基础和代码

---
+++ src/main/java/com/baizhi/TestGit.java
@@ -0,0 +1,10 @@
+package com.baizhi;
+
+/**
+ * Created by Administrator on 2018/7/3.
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("成功！！");
+    }
+}
